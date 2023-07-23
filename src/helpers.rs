941fd38f73b862dcff554a1c05ef99722ace7187
Austin Palmer <44661648+austinjp17@users.noreply.github.com>
2023-07-23 13:40:48 -0500

    docs

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,11 +1,7 @@
 use reqwest;
 
-
-
 pub async fn make_request(url: &str, _client: reqwest::Client) -> Result<String, reqwest::Error> {
     
-    // println!("REQUEST");
-    // Send the GET request and await the response
     let response = _client.get(url).send().await?;
 
     let mut json_data = String::new();
@@ -13,11 +9,10 @@ pub async fn make_request(url: &str, _client: reqwest::Client) -> Result<String,
     if response.status().is_success() {
         // Get the JSON data from the response body
         json_data = response.text().await?;
-        // println!("API Response: {}", json_data);
 
     } else {
-        // Print an error message if the API call was not successful
         println!("API call failed with status code: {}", response.status());
+        Err()
     }
 
     Ok(json_data)
